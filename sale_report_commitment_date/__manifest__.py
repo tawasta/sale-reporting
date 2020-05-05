54fe8ce4bbd7a68c58dc5ea54dbe9569899dcc6f
retropikzel <retropikzel@mailbox.org>
2020-05-05 10:26:48 +0300

    [FIX] Date format, field title

--- sale_report_commitment_date/__manifest__.py
+++ sale_report_commitment_date/__manifest__.py
@@ -20,7 +20,7 @@
 
 {
     "name": "Sale Report - Commitment Date",
-    "summary": "Add commitment date to sale report with title: Confirmed Date",
+    "summary": "Add commitment date to sale report",
     "category": "Sale",
     "version": "12.0.1.0.0",
     "website": "https://github.com/Tawasta/sale-workflow",
