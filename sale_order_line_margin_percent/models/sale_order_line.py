b08ca637c6f320e01cd04c8e3990ab27c9d13120
Jarmo Kortetjärvi <jarmo.kortetjarvi@gmail.com>
2020-12-31 08:58:30 +0200

    [FIX] Logic error in compute loop

--- sale_order_line_margin_percent/models/sale_order_line.py
+++ sale_order_line_margin_percent/models/sale_order_line.py
@@ -6,16 +6,18 @@ from odoo.addons import decimal_precision as dp
 
 class SaleOrderLine(models.Model):
 
-    _inherit = 'sale.order.line'
+    _inherit = "sale.order.line"
 
     margin_percent = fields.Float(
-        string='Margin (%)',
-        digits=dp.get_precision('Margin'),
-        compute='_compute_margin_percent',
+        string="Margin (%)",
+        digits=dp.get_precision("Margin"),
+        compute="_compute_margin_percent",
     )
 
     def _compute_margin_percent(self):
         for record in self:
+            margin_percent = 0
             if record.price_subtotal:
                 margin_percent = (record.margin / record.price_subtotal) * 100
-                record.margin_percent = margin_percent
+
+            record.margin_percent = margin_percent
