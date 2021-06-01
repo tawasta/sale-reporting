9191bdf836e3d3bf51b9c5df6555b35ea00ea120
difbit <timo.kekalainen@hotmail.com>
2021-06-01 13:15:04 +0300

    [MIG] Migration to 14.0

---
+++ sale_auditlog_rules/__manifest__.py
@@ -0,0 +1,39 @@
+##############################################################################
+#
+#    Author: Oy Tawasta OS Technologies Ltd.
+#    Copyright 2019 Oy Tawasta OS Technologies Ltd. (https://tawasta.fi)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see http://www.gnu.org/licenses/agpl.html
+#
+##############################################################################
+
+{
+    "name": "Audit Log Rules - Sales",
+    "summary": "Adds audit log rules for sale order and sale order line",
+    "author": "Tawasta",
+    "license": "AGPL-3",
+    "website": "https://github.com/Tawasta/sale-reporting",
+    "category": "Logging",
+    "application": False,
+    "installable": True,
+    "version": "14.0.1.1.0",
+    "depends": [
+        "sale",
+        "auditlog",
+    ],
+    "data": [
+        "data/rules.xml"
+    ],
+    "demo": [],
+}
