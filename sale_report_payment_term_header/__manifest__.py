1591acf079c34d78b2cb6d0c8be2ab5f26564415
difbit <timo.kekalainen@hotmail.com>
2021-11-03 08:49:56 +0200

    [IMP] Updated version number

--- sale_report_payment_term_header/__manifest__.py
+++ sale_report_payment_term_header/__manifest__.py
@@ -1,7 +1,7 @@
 ##############################################################################
 #
 #    Author: Oy Tawasta OS Technologies Ltd.
-#    Copyright 2021 Oy Tawasta OS Technologies Ltd. (https://tawasta.fi)
+#    Copyright 2020 Oy Tawasta OS Technologies Ltd. (https://tawasta.fi)
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -21,7 +21,7 @@
 {
     "name": "Sale Order report - Payment term in header",
     "summary": "Sale Order report - Payment term in header",
-    "version": "14.0.1.0.0",
+    "version": "12.0.1.0.0",
     "category": "Reporting",
     "website": "https://gitlab.com/tawasta/sale-reporting",
     "author": "Tawasta",
