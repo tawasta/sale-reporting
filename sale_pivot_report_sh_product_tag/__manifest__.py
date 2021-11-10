a2bee3743019d9c6eed6410a79aa2c44dd496286
Timo Kekäläinen <timo.kekalainen@hotmail.com>
2021-11-10 08:16:15 +0000

    12.0 dev sale reporting sh product tag

---
+++ sale_pivot_report_sh_product_tag/__manifest__.py
@@ -0,0 +1,33 @@
+##############################################################################
+#
+#    Author: Oy Tawasta OS Technologies Ltd.
+#    Copyright 2021 Oy Tawasta OS Technologies Ltd. (https://tawasta.fi)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see http://www.gnu.org/licenses/agpl.html
+#
+##############################################################################
+
+{
+    "name": "Sale Analysis - Group by SH product tags",
+    "summary": "Group by SH product tags in Sale analysis pivot report",
+    "version": "12.0.1.0.0",
+    "category": "Reporting",
+    "website": "https://gitlab.com/tawasta/odoo/sale-reporting",
+    "author": "Tawasta",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": ["sh_product_variant_tags", "sale"],
+    "data": ["views/sale_order.xml"],
+}
