fbcf7bcd9bd3928a5a496b7dede1bf096b479dfd
difbit <timo.kekalainen@hotmail.com>
2022-09-08 12:28:23 +0300

    [IMP] Removed defaults, since they kept misbehaving

--- sale_report_quantity_decimals/models/res_config_settings.py
+++ sale_report_quantity_decimals/models/res_config_settings.py
@@ -8,6 +8,5 @@ class ResConfigSettings(models.TransientModel):
     sale_report_decimal_precision = fields.Integer(
         related="company_id.sale_report_decimal_precision",
         readonly=False,
-        default=2,
         store=True,
     )
