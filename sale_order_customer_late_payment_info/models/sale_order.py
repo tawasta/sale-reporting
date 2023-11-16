e8cac633f538d9d523d456c6f96b46506f02abbb
Timo Kekäläinen <timo.kekalainen@hotmail.com>
2023-11-16 14:24:58 +0000

    [IMP] sale_order_customer_late_payment_info module change:

--- sale_order_customer_late_payment_info/models/sale_order.py
+++ sale_order_customer_late_payment_info/models/sale_order.py
@@ -1,4 +1,8 @@
+import logging
+
 from odoo import _, api, fields, models
+
+_logger = logging.getLogger(__name__)
 
 
 class SaleOrder(models.Model):
@@ -7,43 +11,47 @@ class SaleOrder(models.Model):
 
     has_late_payments = fields.Boolean(
         string="Payments are late",
-        compute=lambda self: self._compute_has_late_payments(),
         store=True,
+        copy=False,
     )
 
-    late_payments_dates = fields.Text(
-        string="Late payment dates",
-        compute=lambda self: self._compute_has_late_payments(),
-    )
+    def _cron_has_late_payments(self, batch):
+        orders = self.env["sale.order"].search([("id", "in", batch)])
+        for order in orders:
+            order.onchange_has_late_payments()
+        return batch, "Success"
 
-    @api.depends("partner_id")
-    def _compute_has_late_payments(self):
+    def cron_has_late_payments(self):
+        orders = self.env["sale.order"].search([]).ids
+        batch_orders = list()
+        interval = 50
+        for x in range(0, len(orders), interval):
+            batch_orders.append(orders[x : x + interval])
+
+        for batch in batch_orders:
+            job_desc = _("Update late payment info for orders: {}").format(batch)
+            self.with_delay(description=job_desc)._cron_has_late_payments(batch)
+
+        _logger.info("Cron late payment info completed")
+
+    @api.onchange("partner_id")
+    def onchange_has_late_payments(self):
         """Check for each order if their customer has any payments due"""
-        for order in self:
-            partner = order.partner_id
-            now_date = fields.Datetime.now().date()
+        partner = self.partner_id
+        now_date = fields.Datetime.now().date()
 
-            payment_dates = ""
-            difference = 0
-            late_payments = False
+        difference = 0
+        late_payments = False
 
-            for invoice in partner.invoice_ids:
-                date_due = invoice.invoice_date_due
-                if date_due:
-                    difference = date_due - now_date
+        for invoice in partner.invoice_ids.filtered(lambda o: o.state == "draft"):
+            date_due = invoice.invoice_date_due
+            if date_due:
+                difference = date_due - now_date
 
-                if difference and difference.days < 0:
-                    late_payments = True
-                    if payment_dates:
-                        payment_dates += "\n{} {}".format(
-                            abs(difference.days), _("days")
-                        )
-                    else:
-                        payment_dates += "{} {}".format(abs(difference.days), _("days"))
+            if difference and difference.days < 0:
+                late_payments = True
 
-            order.late_payments_dates = payment_dates
-
-            if late_payments:
-                order.has_late_payments = True
-            else:
-                order.has_late_payments = False
+        if late_payments:
+            self.has_late_payments = True
+        else:
+            self.has_late_payments = False
