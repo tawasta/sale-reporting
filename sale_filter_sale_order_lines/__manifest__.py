a6d681950c9523e9796ee499115832f91481aac0
odooProg <kalle.rantalainen01@gmail.com>
2024-05-23 12:24:48 +0300

    [ADD] new module to filter sale.order.line records

---
+++ sale_filter_sale_order_lines/__manifest__.py
@@ -0,0 +1,35 @@
+##############################################################################
+#
+#    Author: Oy Tawasta OS Technologies Ltd.
+#    Copyright 2024 Oy Tawasta OS Technologies Ltd. (http://www.tawasta.fi)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see http://www.gnu.org/licenses/agpl.html
+#
+##############################################################################
+
+{
+    "name": "Sale Filter Sale Order Lines",
+    "summary": "Filter sale.order.line records and display them in list view",
+    "version": "14.0.1.0.0",
+    "category": "Sales",
+    "website": "https://gitlab.com/tawasta/odoo/sale-reporting",
+    "author": "Tawasta",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": ["sale"],
+    "data": ["views/sale_order_line.xml",
+             "security/ir.model.access.csv"],
+    'post_init_hook': 'create_cron_job'
+}
