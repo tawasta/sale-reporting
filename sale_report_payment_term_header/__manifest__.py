3281a1a50f73a3b255099b152110eff2308a33c3
difbit <timo.kekalainen@tawasta.fi>
2025-05-21 08:18:57 +0300

    [IMP] sale_report_payment_term_header module: Use sudo for payment term info

--- sale_report_payment_term_header/__manifest__.py
+++ sale_report_payment_term_header/__manifest__.py
@@ -21,7 +21,7 @@
 {
     "name": "Sale Order report - Payment term in header",
     "summary": "Sale Order report - Payment term in header",
-    "version": "17.0.1.0.1",
+    "version": "17.0.1.0.2",
     "category": "Reporting",
     "website": "https://github.com/tawasta/sale-reporting",
     "author": "Futural",
