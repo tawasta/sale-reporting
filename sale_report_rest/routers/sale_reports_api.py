a9d1248d88a412e3a2ce6524fdd1528a171ef5d3
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2026-02-18 07:48:40 +0200

    [IMP] - added field

--- sale_report_rest/routers/sale_reports_api.py
+++ sale_report_rest/routers/sale_reports_api.py
@@ -114,6 +114,10 @@ async def invoice_report(
             else line.quantity or 0.0
         )
 
+        discount_amount = (line.price_unit * line.quantity) * (
+            line.discount / 100
+        )
+
         rows.append(
             {
                 "id": line.id,
@@ -129,6 +133,7 @@ async def invoice_report(
                 "commercial_partner": line.commercial_partner_id.name,
                 "partner": line.move_partner_id.name,
                 "discount": line.discount or 0.0,
+                "discount_amount": discount_amount,
                 "discount_amount_currency": line.discount_amount_currency or 0.0,
                 "price_unit": line.price_unit or 0.0,
                 "price_subtotal": line.price_subtotal or 0.0,
