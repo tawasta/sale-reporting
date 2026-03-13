cd21192e7f999cb9989c386fa1a35d214f05619b
Joonas Lahtinen <joonas.lahtinen@futural.fi>
2026-03-13 10:35:31 +0200

    [IMP] Added settings toggle to set visibility of overdue interest in sales reports

---
+++ sale_report_partner_overdue_interest/models/res_config_settings.py
@@ -0,0 +1,10 @@
+from odoo import fields, models, _
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = 'res.config.settings'
+
+    show_overdue_interest = fields.Boolean(
+        string=_("Show overdue interest in sale report"),
+        config_parameter='sale_report_partner_overdue_interest.show_overdue_interest',
+        help="If selected, displays overdue interest in the sale order report"
+    )
