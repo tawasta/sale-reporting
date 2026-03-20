b1245da0eb33bba8f261e0e984ff37b1c521232d
Joonas Lahtinen <joonas.lahtinen@futural.fi>
2026-03-20 14:55:07 +0200

    [FIX] Swapped the settings toggle logic and updated the translations

--- sale_report_partner_overdue_interest/models/res_config_settings.py
+++ sale_report_partner_overdue_interest/models/res_config_settings.py
@@ -5,6 +5,6 @@ class ResConfigSettings(models.TransientModel):
     _inherit = "res.config.settings"
 
     show_overdue_interest = fields.Boolean(
-        string="Show overdue interest in sale report",
+        string="Hide overdue interest in sale order report",
         config_parameter="sale_report_partner_overdue_interest.show_overdue_interest",
     )
